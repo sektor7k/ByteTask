66e4bcd029cec51b0e7040b07d1e93966646ef7d
omer aydin <omeraydin2112@gmail.com>
2023-11-28 04:13:03 +0300

    profile page templates added

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,9 +1,16 @@
+import Navbar from "@/components/Navbar";
 
 
-export default function Profile() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+export default function Profile(){
+
+  return(
+    <>
+    <Navbar/>
+    <div style={{ backgroundColor: '#1E1B24' }} className="min-h-screen flex items-center justify-center">
+      
+    
+
+    </div>
+    </>
+  )
+}
