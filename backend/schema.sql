a9135aabd2aa7d82cc40d687c7cb74500e11b973
omer aydin <omeraydin2112@gmail.com>
2023-12-06 02:10:08 +0300

    created users table from database

---
+++ backend/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE bytetask;
+USE bytetask;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
