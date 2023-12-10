f271e4ccd9550047462ebab7eba87d3aa6064ac0
omer aydin <omeraydin2112@gmail.com>
2023-12-10 07:01:59 +0300

    sql update

--- backend/schema.sql
+++ backend/schema.sql
@@ -5,5 +5,21 @@ CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  status BOOLEAN DEFAULT false
 );
+
+CREATE TABLE userAbout (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  userId INT,
+  about TEXT,
+  FOREIGN KEY (userId) REFERENCES users(id)
+);
+
+
+
+
+
+
+
+
