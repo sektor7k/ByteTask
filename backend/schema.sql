2452e97ab5e7ce4cecedb6374eca5eed668dffa6
omer aydin <omeraydin2112@gmail.com>
2023-12-18 09:10:32 +0300

    database add orders

--- backend/schema.sql
+++ backend/schema.sql
@@ -16,17 +16,30 @@ CREATE TABLE userAbout (
   FOREIGN KEY (userId) REFERENCES users(id)
 );
 
-CREATE TABLE userAbout (
+CREATE TABLE jobs (
   id INT AUTO_INCREMENT PRIMARY KEY,
   userId INT,
   jobTitle VARCHAR(50),
   jobDescription TEXT,
   jobPrice INT,
   workTime INT,
+  revision INT,
 
   FOREIGN KEY (userId) REFERENCES users(id)
 );
 
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    jobId INT,
+    freelancerId INT,
+    customerId INT,
+    customerNote TEXT,
+    orderAmount DECIMAL(10, 2),
+    status VARCHAR(255) DEFAULT 'beklemede',
+    FOREIGN KEY (freelancerId) REFERENCES users(id),
+    FOREIGN KEY (customerId) REFERENCES users(id),
+    FOREIGN KEY (jobId) REFERENCES jobs(id)
+);
 
 
 
